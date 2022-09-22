4a84d7c126e22f62d1286ca9dc001b1a312ef5e6
wenxiaofang2022 <wenxiaofang.jiayou@gmail.com>
2022-09-22 09:54:50 +0800

    feat:油量增加效果

--- nuxt/static/1663759042/daily/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663759042",layout:"default",error:a,state:{counter:0},serverRendered:true,routePath:"\u002Fdaily",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
