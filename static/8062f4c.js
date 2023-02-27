7baee60cbff9e48d7ca1ddc911d203f14d98baca
wenxiaofang2022 <wenxiaofang.jiayou@gmail.com>
2023-02-27 12:19:59 +0800

    fix:修改

---
+++ static/8062f4c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{504:function(n,o,r){"use strict";r.r(o);o.default="\n\nuniform sampler2D uMap;\n\nvarying vec2 vUv;\nvarying float vDiff;\n\nvoid main() {\n\tgl_FragColor = texture2D(uMap, vUv);\n}"}}]);
