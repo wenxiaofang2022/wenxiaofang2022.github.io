3bfe6ba78cd57f82efedfce7436b6fc50b25dc55
wenxiaofang2022 <wenxiaofang.jiayou@gmail.com>
2023-05-30 20:22:09 +0800

    fix:新增json部分逻辑

--- js/index.js
+++ js/index.js
@@ -2,8 +2,10 @@ var targets  = $(".projects-box .project-slide .content");
 var step = -316;
 var sum = 0;
 var times = 0;
-var lines = 5;
-console.log($(".projects-box .project-slide .content .slider").find("a"));
+// var lines = 8;
+var lines = Math.ceil(LinkData.data.length/8);
+// console.log("lines",lines);
+// console.log($(".projects-box .project-slide .content .slider").find("a"));
 var timer = setInterval(() => {
   sum = step + sum;
   // times++;
@@ -31,7 +33,8 @@ var timer = setInterval(() => {
       $(".projects-box .project-slide").eq(7).find(".content .slider a").eq(m).css("opacity","1");
     }
   }
-  if(times==4||times>4){
+  if(times==lines-1||times>lines-1){
+  // if(times==7||times>7){
     sum = 0;
     times = 0;
     $(".projects-box .project-slide .content").css("transform","translateY("+sum+"px)").css("transition","all 0s ease-out");;
